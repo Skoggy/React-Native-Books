ebc1b16af7d19389dcbccb2af9653da49a48b4c4
Chris Skogstad <chris.skogstad1@gmail.com>
2021-04-09 23:18:25 +1000

    can now create, find all and find by id with insomnia for testing purposes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,7 +14,7 @@ app.use(cors());
 //     app.use(express.static("client/build"));
 // }
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
